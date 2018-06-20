5f156aec8bded68c3b4c81e23b60a74526fab391
markerlnwz <zxcvbnm_3456@hotmail.com>
2018-06-20 23:43:32 +0700

    fixed source .
    fixed daemon bitcoinprod crashed.

---
+++ src/onionseed.h
@@ -0,0 +1,23 @@
+
+#ifndef STEALTH_ONIONSEED_H
+#define STEALTH_ONIONSEED_H
+
+// hidden service seeds
+static const char *strMainNetOnionSeed[][1] = {
+    {"ilhx3kycrozgv6fp.onion"},
+    {"cgjvkdwupxk34357.onion"},
+	{"2ldtbzfsaciodlcr.onion"},
+	{"iuhevxbaxszqnuzs.onion"},
+	{"veu7qyqygsh4qavs.onion"},
+	{"t7gudn4hww3rztlw.onion"},
+	{"ogwykxznqzsne6tx.onion"},
+	{"ahtr2xnyhlyvlq2f.onion"},
+	{"2ldtbzfsaciodlcr.onion"},
+    {NULL}
+};
+
+static const char *strTestNetOnionSeed[][1] = {
+    {NULL}
+};
+
+#endif
